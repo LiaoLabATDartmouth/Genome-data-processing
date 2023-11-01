72b6c04677a7282a410068195e78c37cc55ed83f
Chen Liao <149118340+LiaoLabATDartmouth@users.noreply.github.com>
2023-11-01 08:59:37 -0400

    Update run_repeatmasker.sh

--- GATK_variant_calling/RepeatMasker/run_repeatmasker.sh
+++ GATK_variant_calling/RepeatMasker/run_repeatmasker.sh
@@ -1,3 +1,2 @@
-# ./RepeatMasker/RepeatMasker -pa 2 -species bacteria -dir output ../reference_genome/CD630.fasta
-# rmsk2bed < output/CD630.fasta.out > output/CD630.fasta.bed
+./RepeatMasker/RepeatMasker -pa 2 -species bacteria -dir output ../reference_genome/CD630.fasta
 python3 ./RepeatMasker/util/RM2Bed.py output/CD630.fasta.out output/CD630.fasta.bed
